c6a1e452cb4c1f01c781a2800656831bc0422784
Rainy <geekrainy@gmail.com>
2016-12-24 19:32:57 +0800

    Complete main feature

--- index.js
+++ index.js
@@ -9,15 +9,24 @@ var imgUrl = hexo.config.imgurl;
  * 
  * Syntax:
  * ```
- * {% imgurl [class1,class2,classN] imagePath %}
+ * {% imgurl imagePath [class1,class2,classN] imgAttr %}
  * ```
  */
 
 hexo.extend.tag.register('imgurl', function(args){
-  
   var imagePath = args[0];
   var classes = args[1] || "";
   var imgAttr = {};
+
+  for (var i = 2; i < args.length; i++) {
+    var item = args[i];
+    if(item[0] === '\'' || item[0] === '"') {
+      item = item.substring(1, item.length - 1);
+    }
+
+    var parseAttr = item.split(':');
+    imgAttr[parseAttr[0]] = parseAttr[1];
+  }
 
   classes = classes.split(',');
   imgAttr.class = classes.join(' ');
